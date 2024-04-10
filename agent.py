f2ec30ed1c96a7f08ee3e5a4f3a18d8930e3ae72
breadbread1984 <breadbread1984@163.com>
2024-04-10 15:57:30 +0800

    update

--- agent.py
+++ agent.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
+from langchain import hub
 from langchain.llms import HuggingFaceEndpoint, OpenAI
-from langchain.agents import load_tools, initialize_agent
+from langchain_openai import ChatOpenAI
+from langchain.chat_models.huggingface import ChatHuggingFace
+from langchain.agents import AgentExecutor, load_tools
+from langchain.agents.format_scratchpad import format_log_to_str
+from langchain.agents.output_parsers import ReActJsonSingleInputOutputParser
+from langchain.tools.render import render_text_description
+from langchain_community.utilities import SerpAPIWrapper
 
 class Agent(object):
   def __init__(self, model_id = 'HuggingFaceH4/zephyr-7b-beta', tools = ["google-serper", "llm-math", "serpapi"]):
@@ -13,10 +20,15 @@ class Agent(object):
                         'SOLAR-10.7B-Instruct-v1.0'}
     if model_id == 'text-davinci-003':
       llm = OpenAI(model_name = model_id, temperature = 0)
+      chat_model = ChatOpenAI(llm = llm)
     else:
       llm = HuggingFaceEndpoint(repo_id = model_id)
+      chat_model = ChatHuggingFace(llm = llm)
     tools = load_tools(tools, llm = llm)
-    self.agent = initialize_agent(tools, llm, agent = "zero-shot-react-description", verbose = True)
+    prompt = hub.pull("hwchase17/react-json")
+    prompt = prompt.partial(tools = render_text_description(tools), tool_names = ", ".join([t.name for t in tools]))
+    agent = {"input": lambda x: x["input"], "agent_scratchpad": lambda x: format_log_to_str(x["intermediate_steps"])} | prompt | chat_model.bind(stop = ["\nObservation"]) | ReActJsonSingleInputOutputParser()
+    self.agent_executor = AgentExecutor(agent = agent, tools = tools, verbose = True)
   def query(self, question):
-    self.run(question)
+    self.aget_executor.invoke({"input": question})
 
