799c53514cd59d57355d1dd48d64fd3effd234c5
breadbread1984 <breadbread1984@163.com>
2024-04-10 14:27:49 +0800

    update

--- agent.py
+++ agent.py
@@ -3,7 +3,8 @@
 from langchain.llms import HuggingFaceEndpoint
 from langchain.chat_models.huggingface import ChatHuggingFace
 
-def get_agent(model_id):
+def get_agent(model_id = 'HuggingFaceH4/zephyr-7b-beta'):
+  # NOTE: an inferior choice is meta-llama/Llama-2-70b-chat-hf
   llm = HuggingFaceEndpoint(repo_id = model_id)
   chat_model = ChatHuggingFace(llm = llm)
   return chat_model
