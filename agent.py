ef90e68d7163d3cc6632c8638d6cb52be2fdc5d0
breadbread1984 <breadbread1984@163.com>
2024-04-11 10:13:23 +0800

    update

--- agent.py
+++ agent.py
@@ -9,20 +9,20 @@ class Agent(object):
   def __init__(self, model = 'zephyr', tools = ["google-serper", "llm-math"], device = 'cuda'):
     assert device in {'cpu', 'cuda'}
     environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
+    environ["SERPAPI_API_KEY"] = 'd075ad1b698043747f232ec1f00f18ee0e7e8663'
     hf_model_list = {'chatglm3': 'THUDM/chatglm3-6b',
                      'llama2': 'meta-llama/Llama-2-7b-chat-hf',
                      'zephyr': "HuggingFaceH4/zephyr-7b-beta",}
-    environ["SERPAPI_API_KEY"] = '052741ba82a96e21b3c3ab35e6c5288f470a11402bc83a9cc86c306f826d24f0'
     if model == 'openai':
       llm = OpenAI(model_name = 'text-davinci-003', temperature = 0)
     else:
       llm = HuggingFaceEndpoint(repo_id = hf_model_list[model], token = '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')
     memory = ConversationBufferMemory(memory_key="chat_history")
-    tools = load_tools(tools, llm = llm, serper_api_key = '052741ba82a96e21b3c3ab35e6c5288f470a11402bc83a9cc86c306f826d24f0')
+    tools = load_tools(tools, llm = llm, serper_api_key = 'd075ad1b698043747f232ec1f00f18ee0e7e8663')
     self.agent_chain = initialize_agent(tools = tools, llm = llm, memory = memory, agent = "zero-shot-react-description", verbose = True)
   def query(self, question):
     return self.agent_chain.run(question)
 
 if __name__ == "__main__":
   agent = Agent(model = 'zephyr', device = 'cpu')
-  print(agent.query("where does the word maelstrom come?"))
+  print(agent.query("list famous actors and actress of hong kong."))
