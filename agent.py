37bcf94ad70644efa79444a4559ed4cb63eedd7d
breadbread1984 <breadbread1984@163.com>
2024-04-11 09:56:26 +0800

    update

--- agent.py
+++ agent.py
@@ -1,39 +1,56 @@
 #!/usr/bin/python3
 
 from os import environ
+import json
+import logging
 from huggingface_hub import login
 from langchain import hub
 from langchain.llms import HuggingFaceEndpoint, OpenAI
 from langchain_openai import ChatOpenAI
 from langchain_community.chat_models.huggingface import ChatHuggingFace
-from langchain.agents import AgentExecutor, load_tools
+from langchain.agents import initialize_agent, AgentExecutor, load_tools
 from langchain.agents.format_scratchpad import format_log_to_str
 from langchain.agents.output_parsers import ReActJsonSingleInputOutputParser
 from langchain.tools.render import render_text_description
 from langchain_community.utilities import SerpAPIWrapper
 from models import ChatGLM3, Llama2, Zephyr
 
+class CustomizedParser(ConvoOutputParser):
+  def parse(self, text: str):
+    try:
+      return super().parse(text)
+    except Exception:
+      logging.exception("Error parsing LLM output: %s", text)
+      try:
+        response = json.loads(text)
+        if "action" in response and "action_input" in response:
+          action, action_input = response["action"], response["action_input"]
+          if action == "Final Answer":
+            return AgentFinish({"output": action_input}, text)
+          else:
+            return AgentAction(action, action_input, text)
+        else:
+          raise OutputParserException(f"Missing 'action' or 'action_input' in LLM output: {text}")
+      except Exception as e:
+        raise OutputParserException(f"Could not parse LLM output: {text}") from e
+
 class Agent(object):
-  def __init__(self, model = 'zephyr', tools = ["google-serper", "llm-math", "serpapi"], device = 'cuda'):
+  def __init__(self, model = 'zephyr', tools = ["llm-math", "serpapi"], device = 'cuda'):
     assert device in {'cpu', 'cuda'}
-    hf_model_list = {'chatglm3': ChatGLM3,
-                     'llama2': Llama2,
-                     'zephyr': Zephyr}
+    environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
+    hf_model_list = {'chatglm3': 'THUDM/chatglm3-6b',
+                     'llama2': 'meta-llama/Llama-2-7b-chat-hf',
+                     'zephyr': "HuggingFaceH4/zephyr-7b-beta",}
     environ["SERPAPI_API_KEY"] = '052741ba82a96e21b3c3ab35e6c5288f470a11402bc83a9cc86c306f826d24f0'
     if model == 'openai':
       llm = OpenAI(model_name = 'text-davinci-003', temperature = 0)
-      chat_model = ChatOpenAI(llm = llm)
     else:
-      llm = hf_model_list[model](device = device)
-      chat_model = ChatHuggingFace(llm = llm)
+      llm = HuggingFaceEndpoint(repo_id = hf_model_list[model], token = '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')
     tools = load_tools(tools, llm = llm, serper_api_key = '052741ba82a96e21b3c3ab35e6c5288f470a11402bc83a9cc86c306f826d24f0')
-    prompt = hub.pull("hwchase17/react-json")
-    prompt = prompt.partial(tools = render_text_description(tools), tool_names = ", ".join([t.name for t in tools]))
-    agent = {"input": lambda x: x["input"], "agent_scratchpad": lambda x: format_log_to_str(x["intermediate_steps"])} | prompt | chat_model.bind(stop = ["\nObservation"]) | ReActJsonSingleInputOutputParser()
-    self.agent_executor = AgentExecutor(agent = agent, tools = tools, verbose = True)
+    self.agent_chain = initialize_agent(tools = tools, llm = llm, agent = "zero-shot-react-description", verbose = True)
   def query(self, question):
-    return self.agent_executor.invoke({"input": question})
+    return self.agent_chain.run(question)
 
 if __name__ == "__main__":
-  agent = Agent(model = 'llama2')
-  print(agent.query("who is Jinping Xi's daughter?"))
+  agent = Agent(model = 'zephyr', device = 'cpu')
+  print(agent.query("where does the word maelstrom come?"))
