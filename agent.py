5477d43270317619629830bf184398b5984a2791
breadbread1984 <breadbread1984@163.com>
2024-04-11 11:27:02 +0800

    update

--- agent.py
+++ agent.py
@@ -16,17 +16,29 @@ class Agent(object):
     environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
     # get token from https://serper.dev/
     environ["SERPAPI_API_KEY"] = 'd075ad1b698043747f232ec1f00f18ee0e7e8663'
-    hf_model_list = {'chatglm3': 'THUDM/chatglm3-6b',
+    openai_model_list = {'ada001': 'text-ada-001',
+                         'babbage001': 'text-babbage-001',
+                         'curie001': 'text-curie-001',
+                         'davinci001': 'text-davinci-001',
+                         'davinci002': 'text-davinci-002',
+                         'davinci003': 'text-davinci-003',
+                         'gpt3.5': 'gpt-3.5-turbo-instruct',
+                         'gpt4': 'gpt-4'}
+    hf_model_list = {'mixtral': 'mistralai/Mixtral-8x7B-Instruct-v0.1',
+                     'mistral': 'teknium/OpenHermes-2.5-Mistral-7B',
+                     'solar': 'upstage/SOLAR-10.7B-Instruct-v1.0',
                      'llama2': 'meta-llama/Llama-2-7b-chat-hf',
                      'zephyr': "HuggingFaceH4/zephyr-7b-beta",}
-    if model == 'openai':
-      llm = OpenAI(model_name = 'text-davinci-003',
+    if model in openai_model_list:
+      llm = OpenAI(model_name = openai_model_list[model],
                    temperature = 0,
                    openai_api_key = getpass(),
                    openai_organization = 'to be filled')
-    else:
+    elif model in hf_model_list:
       llm = HuggingFaceEndpoint(repo_id = hf_model_list[model],
                                 token = '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')
+    else:
+      raise Exception('unknown model!')
     memory = ConversationBufferMemory(memory_key="chat_history")
     tools = load_tools(tools, llm = llm, serper_api_key = 'd075ad1b698043747f232ec1f00f18ee0e7e8663')
     self.agent_chain = initialize_agent(tools = tools, llm = llm, memory = memory, agent = "zero-shot-react-description", verbose = True)
