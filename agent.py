6fa1efdc19ef40d58448b36ec5f661f7357c178b
breadbread1984 <breadbread1984@163.com>
2024-04-11 10:04:08 +0800

    update

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from os import environ
+from langchain.memory import ConversationBufferMemory
 from langchain.llms import HuggingFaceEndpoint, OpenAI
 from langchain.agents import initialize_agent, load_tools
 
@@ -16,8 +17,9 @@ class Agent(object):
       llm = OpenAI(model_name = 'text-davinci-003', temperature = 0)
     else:
       llm = HuggingFaceEndpoint(repo_id = hf_model_list[model], token = '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')
+    memory = ConversationBufferMemory(memory_key="chat_history")
     tools = load_tools(tools, llm = llm, serper_api_key = '052741ba82a96e21b3c3ab35e6c5288f470a11402bc83a9cc86c306f826d24f0')
-    self.agent_chain = initialize_agent(tools = tools, llm = llm, agent = "zero-shot-react-description", verbose = True)
+    self.agent_chain = initialize_agent(tools = tools, llm = llm, memory = memory, agent = "zero-shot-react-description", verbose = True)
   def query(self, question):
     return self.agent_chain.run(question)
 
