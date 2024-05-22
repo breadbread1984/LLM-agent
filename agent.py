34b9b443cf13fa38c644d05d324f089bf46225a1
breadbread1984 <breadbread1984@163.com>
2024-05-22 09:52:23 +0800

    update

--- agent.py
+++ agent.py
@@ -3,48 +3,28 @@
 from os import environ
 from getpass import getpass
 from langchain.memory import ConversationBufferMemory
-from langchain.llms import HuggingFaceEndpoint, OpenAI
 from langchain.agents import AgentType, initialize_agent, load_tools
+from models import Llama3, CodeLlama
+from prompts import agent_template
 
 class Agent(object):
-  def __init__(self, model = 'zephyr', tools = ["google-serper", "llm-math", "wikipedia", "arxiv"]):
-    # openai is not available in china, cannot singup for an api key
-    # get token from https://platform.openai.com/overview
-    environ['OPENAI_API_KEY'] = 'to be filled'
-    environ['OPENAI_ORGANIZATION'] = 'HKQAI'
-    # get token from https://huggingface.co/
-    environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
-    # get token from https://serper.dev/
-    environ["SERPAPI_API_KEY"] = 'd075ad1b698043747f232ec1f00f18ee0e7e8663'
-    openai_model_list = {'ada001': 'text-ada-001',
-                         'babbage001': 'text-babbage-001',
-                         'curie001': 'text-curie-001',
-                         'davinci001': 'text-davinci-001',
-                         'davinci002': 'text-davinci-002',
-                         'davinci003': 'text-davinci-003',
-                         'gpt3.5': 'gpt-3.5-turbo-instruct',
-                         'gpt4': 'gpt-4'}
-    hf_model_list = {'mixtral': 'mistralai/Mixtral-8x7B-Instruct-v0.1',
-                     'mistral': 'teknium/OpenHermes-2.5-Mistral-7B',
-                     'solar': 'upstage/SOLAR-10.7B-Instruct-v1.0',
-                     'llama2': 'meta-llama/Llama-2-7b-chat-hf',
-                     'zephyr': "HuggingFaceH4/zephyr-7b-beta",}
-    if model in openai_model_list:
-      llm = OpenAI(model_name = openai_model_list[model],
-                   temperature = 0,
-                   openai_api_key = getpass(),
-                   openai_organization = 'to be filled')
-    elif model in hf_model_list:
-      llm = HuggingFaceEndpoint(repo_id = hf_model_list[model],
-                                token = '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')
+  def __init__(self, model = 'llama3', tools = ["google-serper", "llm-math", "wikipedia", "arxiv"], locally = False):
+    if model == 'llama3':
+      tokenizer, llm = Llama3(locally)
+    elif model == 'codellama':
+      tokenizer, llm = CodeLlama(locally)
     else:
       raise Exception('unknown model!')
-    memory = ConversationBufferMemory(memory_key="chat_history")
     tools = load_tools(tools, llm = llm, serper_api_key = 'd075ad1b698043747f232ec1f00f18ee0e7e8663')
-    self.agent_chain = initialize_agent(tools = tools, llm = llm, memory = memory, agent = AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose = True)
+    prompt = agent_template(tokenizer, tools)
+    llm_with_stop = llm.bind(stop = ["\nObservation"])
+    chain = {"input": lambda x: x["input"], "agent_scratchpad": lambda x: format_log_to_str(x["intermediate_steps"])} | prompt | llm_with_stop | ReactJsonSingleInputOutputParser()
+
+    memory = ConversationBufferMemory(memory_key="chat_history")
+    self.agent_chain = AgentExecutor(agent = chain, tools = tools, memory = memory, verbose = True)
   def query(self, question):
-    return self.agent_chain.run(question)
+    return self.agent_chain.invoke({"input": question})
 
 if __name__ == "__main__":
-  agent = Agent(model = 'zephyr')
+  agent = Agent(model = 'llama3')
   print(agent.query("what is SwiGLU?"))
