85624f721184666e1ba125991ff8b79aba525fde
breadbread1984 <breadbread1984@163.com>
2024-05-22 17:24:41 +0800

    update

--- tools.py
+++ tools.py
@@ -2,7 +2,7 @@
 
 from os import environ
 from typing import Optional, Type
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, PreTrainedTokenizerFast
 from langchain.pydantic_v1 import BaseModel, Field
 from langchain.tools import BaseTool, StructuredTool, Tool, tool
 from langchain.graphs import Neo4jGraph
@@ -14,8 +14,10 @@ class ChemKnowledgeInput(BaseModel):
   query: str = Field(description = "should be a search query")
 
 class ChemKnowledgeConfig(BaseModel):
+  class Config:
+    arbitrary_types_allowed = True
   neo4j: Neo4jGraph
-  tokenizer: AutoTokenizer
+  tokenizer: PreTrainedTokenizerFast
   llm: HuggingFaceEndpoint
 
 class ChemKnowledgeTool(BaseTool):
@@ -24,15 +26,13 @@ class ChemKnowledgeTool(BaseTool):
   args_schema: Type[BaseModel] = ChemKnowledgeInput
   return_direct: bool = True
   config: ChemKnowledgeConfig
-  def __init__(self, config: ChemKnowledgeConfig):
-    self.config = config
   def _run(self, query: str, run_manager: Optional[CallbackManagerForToolRun] = None) -> str:
     # 1) extract entities of known entity types
     results = self.config.neo4j.query("match (n) return distinct labels(n)")
     entity_types = [result['labels(n)'][0] for result in results]
     prompt = entity_generation_template(self.config.tokenizer, entity_types)
     chain = prompt | self.config.llm
-    entities = chain.invoke({'question': question})
+    entities = chain.invoke({'question': query})
     entities = eval(entities)
     print('extracted entityes:', entities)
     # 2) search for triplets related to these triplets
@@ -52,27 +52,28 @@ class ChemKnowledgeTool(BaseTool):
     # 3) ask llm for answer according to matched triplets
     prompt = triplets_qa_template(self.config.tokenizer, triplets)
     chain = prompt | self.config.llm
-    answer = chain.invoke({'question': question})
+    answer = chain.invoke({'question': query})
     return answer
   async def _arun(self, query: str, run_manager: Optional[AsyncCallbackManagerForToolRun] = None) -> str:
     raise NotImplementedError("Chemistry Knowledge Graph does not suppert async!")
 
 if __name__ == "__main__":
   environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
-  config = ChemKnowledgeConfig(
-    neo4j = Neo4jGraph(url = 'bolt://localhost:7687', username = 'neo4j', password = '19841124', database = 'neo4j'),
-    tokenizer = AutoTokenizer.from_pretrained('meta-llama/Meta-Llama-3-8B-Instruct'),
-    llm = HuggingFaceEndpoint(
-      endpoint_url = "meta-llama/Meta-Llama-3-8B-Instruct",
-      task = "text-generation",
-      max_length = 16384,
-      do_sample = False,
-      temperature = 0.6,
-      top_p = 0.9,
-      eos_token_id = [tokenizer.eos_token_id, tokenizer.convert_tokens_to_ids("<|eot_id|>")],
-      use_cache = True,
-    )
+  neo4j = Neo4jGraph(url = 'bolt://localhost:7687', username = 'neo4j', password = '19841124', database = 'neo4j')
+  tokenizer = AutoTokenizer.from_pretrained('meta-llama/Meta-Llama-3-8B-Instruct')
+  llm = HuggingFaceEndpoint(
+    endpoint_url = "meta-llama/Meta-Llama-3-8B-Instruct",
+    task = "text-generation",
+    max_length = 16384,
+    do_sample = False,
+    temperature = 0.6,
+    top_p = 0.9,
+    eos_token_id = [tokenizer.eos_token_id, tokenizer.convert_tokens_to_ids("<|eot_id|>")],
+    use_cache = True,
   )
-  kb = ChemKnowledgeTool(config = config)
+  kb = ChemKnowledgeTool(config = ChemKnowledgeConfig(neo4j = neo4j, tokenizer = tokenizer, llm = llm))
+  print(kb.name)
+  print(kb.description)
+  print(kb.args)
   res = kb('what is the application of sodium chloride?')
   print(res)
